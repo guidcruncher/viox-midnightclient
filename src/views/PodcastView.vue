88eb33d71db35fe24293d69e96ef8e1493f72bb2
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-04-12 03:29:14 +0100

    Scaffolding

--- src/views/PodcastView.vue
+++ src/views/PodcastView.vue
@@ -5,7 +5,6 @@ import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 
 import { ApiClient } from '@/api'
-import { usePlayer } from '@/composables/usePlayer'
 import { getQueryString } from '@/utils/getQueryString'
 
 interface LoadMoreEvent {
@@ -14,7 +13,6 @@ interface LoadMoreEvent {
   done: (moreAvailable: boolean) => void
 }
 
-const { currentTrack, playItem } = usePlayer()
 const items = ref<MediaItem[]>([])
 const isLoading = ref(false)
 const podcastId = ref('')
@@ -54,45 +52,22 @@ const handleAdd = async () => {
   await ApiClient.addToLibrary(podcastId.value)
 }
 
-const handleSelect = async (item: any) => {
-  await playItem(item, podcast.value?.sourceRef.sourceId)
+const handlePlayAll = async () => {
+  await ApiClient.play(podcastId.value)
 }
 </script>
 
 <template>
   <div v-if="ready">
-    <div class="flex items-center justify-between">
-      <button
-        @click.stop="handleAdd"
-        class="right-3 top-3 z-20 flex h-8 w-8 items-center justify-center rounded-full bg-black/60 backdrop-blur-sm text-white hover:bg-black/80 transition"
-      >
-        <LucideIcon name="Star" :size="18" class="text-yellow-400" />
-      </button>
-    </div>
-
-    <div class="paged-list-wrapper">
-      <PagedList
-        :items="items"
-        :loading="isLoading"
-        :page-size="pageSize"
-        @load-more="handleLoadMore"
-      >
-        <div
-          class="grid grid-cols-3 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
-        >
-          <SquareCard
-            v-for="item in items"
-            :key="item.id"
-            :item="item"
-            :active="item.id === currentTrack?.id"
-            :imageUrl="item.imageUrl"
-            :itemType="item.sourceRef.itemType"
-            :title="item.title"
-            :subtitle="item.subtitle"
-            @click="handleSelect"
-          />
-        </div>
-      </PagedList>
-    </div>
+    <TrackList
+      v-if="podcast && items"
+      :parent="podcast"
+      :tracks="items"
+      :loading="isLoading"
+      :page-size="pageSize"
+      @favorite="handleAdd"
+      @playall="handlePlayAll"
+      @load-more="handleLoadMore"
+    />
   </div>
 </template>
