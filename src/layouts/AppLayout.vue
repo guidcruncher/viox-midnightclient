62aff6e700e828d868ebc25daefe36bf1f30342a
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-04-23 11:25:43 +0100

    Layout fixs

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -10,7 +10,7 @@ const { showFullPlayer } = usePlayer()
     <BackgroundEffects />
     <SidebarNav />
 
-    <main class="relative order-1 flex-1 overflow-y-auto overflow-x-hidden p-4 z-10 md:order-2 md:p-8 flex flex-col">
+    <main class="relative order-1 flex-1 overflow-y-auto overflow-x-hidden p-4 z-10 md:order-2 md:p-4 flex flex-col">
       
       <div class="mx-auto w-full max-w-7xl pb-32 flex-1 flex flex-col">
         
